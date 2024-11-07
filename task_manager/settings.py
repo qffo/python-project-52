2cb3154ad57c0fb30b7158519b91743679624d81
qffo <075viktor@mail.ru>
2024-11-07 20:16:20 +0300

    refactro(/settings.py) remove DATABASE_URL in .env

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600
     )
 }
