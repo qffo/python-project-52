037df80cca1680f818804d9f2a910c76392a039b
qffo <075viktor@mail.ru>
2024-11-10 21:21:24 +0300

    refactor(urls) add users
    начал создаваь отображение пользователей, добавил users/

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,6 +20,7 @@ from task_manager import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('users/', views.user_list, name='user_list'),
 ]
