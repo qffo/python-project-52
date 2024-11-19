27c18947c606f1aeb010e2733566ddac1223c0a8
qffo <075viktor@mail.ru>
2024-11-19 22:51:20 +0300

    refactor(task_manager/statuses/forms.py) added the default display

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -6,3 +6,7 @@ class StatusForm(forms.ModelForm):
     class Meta:
         model = Status
         fields = ['name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].initial = 'Имя'
