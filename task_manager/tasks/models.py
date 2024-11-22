e637b40b1d3c0636da9914d26d27dcca806189bd
qffo <075viktor@mail.ru>
2024-11-22 20:23:24 +0300

    add(/tasks/models.py): created a model for the task

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,23 +1,17 @@
-# from django.db import models
+from django.db import models
 
-# from task_manager.statuses.models import Status
-
-
-# class Task(models.Model):
-#     name = models.CharField(max_length=255,
-#                             blank=False,
-#                             unique=True)
-#     description = models.TextField(blank=False)
+from task_manager.statuses.models import Status
+from task_manager.users.models import User
 
 
-#     author = models.ForeignKey(User, on_delete=models.PROTECT)
-#     status = models.ForeignKey(Status, on_delete=models.PROTECT)
-#     executor = models.ForeignKey(User, on_delete=models.PROTECT,
-#                                  verbose_name=_('Executor'),
-#                                  related_name='Executor')
-#     labels = models.ManyToManyField(Label, blank=True,
-#                                     verbose_name=_('Labels'))
-#     created_at = models.DateTimeField(auto_now_add=True)
+class Task(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    description = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    executor = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name='Executor')
+    created_at = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
