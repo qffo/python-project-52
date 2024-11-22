c41913bbbc55647a3bfa2aaf832b1e04d5a9b350
qffo <075viktor@mail.ru>
2024-11-22 20:25:21 +0300

    refactor(/tasks/views.py): added task display and creation

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
+from task_manager.tasks.models import Task
+from pyexpat.errors import messages
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from task_manager.tasks.forms import TaskCreationForm
+from task_manager.statuses.models import Status
+from task_manager.users.models import User
 
 
 def index(request):
     return render(request, 'tasks/tasks_list.html')
+
+
+def tasks_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/tasks_list.html', {'tasks': tasks})
+
+
+class TaskCreateView(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request):
+        form = TaskCreationForm()
+        statuses = Status.objects.all()
+        users = User.objects.all()
+        return render(request, 'tasks/task_create.html', {
+            'form': form,
+            'statuses': statuses,
+            'users': users
+        })
+
+    def post(self, request):
+        form = TaskCreationForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author = request.user
+            task.save()
+            messages.success(request, "Задача успешно создана")
+            return redirect('tasks_list')
+        messages.error(request, "Ошибка при создании задачи")
+        return render(request, 'tasks/task_create.html', {'form': form})
