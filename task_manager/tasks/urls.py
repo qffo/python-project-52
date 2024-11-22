d4a281c15fd8bcb1ab414d7fa45d08763af99b13
qffo <075viktor@mail.ru>
2024-11-22 20:24:12 +0300

    refactor(/tasks/urls.py): added a link to create a task

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from task_manager.tasks import views
 
 urlpatterns = [
-    path('', views.index, name='tasks_list'),
+    path('', views.tasks_list, name='tasks_list'),
+    path('create/', views.TaskCreateView.as_view(), name='task_create'),
 ]
