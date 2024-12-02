4314bd4440753c2ba219d52caefdaf9a25913458
qffo <075viktor@mail.ru>
2024-12-02 21:34:23 +0300

    refactor(/settings.py) for test

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': dj_database_url.config(
 #         # Replace this value with your local database's connection string.
@@ -104,11 +104,11 @@ DATABASES = {
 #         conn_max_age=600
 #     )
 # }
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         conn_max_age=600,
-#         default='sqlite:///db.sqlite3')
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        default='sqlite:///db.sqlite3')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
