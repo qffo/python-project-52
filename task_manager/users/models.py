9b86ba6ccd8e0bad18d3fab427290e2971e3e5cf
qffo <075viktor@mail.ru>
2024-12-03 18:40:38 +0300

    refactor(/users/models.py): add django.utils.translation in projec

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    first_name = models.CharField(blank=False, max_length=150)
-    last_name = models.CharField(blank=False, max_length=150)
+    first_name = models.CharField(_('First name'), blank=False, max_length=150)
+    last_name = models.CharField(_('Last name'), blank=False, max_length=150)
     USERNAME_FIELD = 'username'
 
     def __str__(self):
