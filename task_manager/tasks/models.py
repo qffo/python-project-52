015668de4cde9606aece6a330f9d39d2ed6326d6
qffo <075viktor@mail.ru>
2024-12-04 15:43:41 +0300

    refactor(/templates/tasks/task_create.html): add bootstrap_form form

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-
 from task_manager.statuses.models import Status
 from task_manager.users.models import User
 from task_manager.labels.models import Label
+from django.utils.translation import gettext_lazy as _
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=255, blank=False, unique=True)
-    description = models.TextField(blank=False)
+    name = models.CharField(_('Name'), max_length=255, blank=False, unique=True)
+    description = models.TextField(_('Description'), blank=False)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
-    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    status = models.ForeignKey(
+        Status, on_delete=models.PROTECT, verbose_name=_('Status'))
     executor = models.ForeignKey(
-        User, on_delete=models.PROTECT, related_name='Executor')
-    labels = models.ManyToManyField(Label, blank=True)
+        User, on_delete=models.PROTECT, related_name='Executor', verbose_name=_('Executor'))
+    labels = models.ManyToManyField(Label, blank=True, verbose_name=_('Labels'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
