e6ab202a3e3d8e5204bf440fa64aad5f978f2807
qffo <075viktor@mail.ru>
2024-12-18 20:46:36 +0300

    refactor(/users/models.py): add f-string

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -9,4 +9,4 @@ class User(AbstractUser):
     USERNAME_FIELD = 'username'
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return f'{self.first_name} {self.last_name}'
