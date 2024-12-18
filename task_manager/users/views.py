4534e2e6b607e285708312f5339aa2f14725d4f2
qffo <075viktor@mail.ru>
2024-12-18 20:47:44 +0300

    refactor(/users/views.py): immediately made a return from the function

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import ProtectedError
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
+from django.views import View
 from django.views.generic import CreateView, UpdateView
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
@@ -23,7 +25,7 @@ class UserCreateView(CreateView):
         return response
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = CustomUserChangeForm
     template_name = 'users/update.html'
@@ -32,13 +34,9 @@ class UserUpdateView(UpdateView):
     def get_object(self, queryset=None):
         user = super().get_object(queryset)
         if user != self.request.user:
-            messages.error(
-                self.request,
-                _("You do not have the rights to change another user.")
-            )
-            raise Http404(
-                _("You cannot edit other people's data.")
-            )
+            messages.error(self.request, _(
+                "You do not have the rights to change another user."))
+            raise Http404(_("You cannot edit other people's data."))
         return user
 
     def get(self, request, *args, **kwargs):
@@ -50,35 +48,36 @@ class UserUpdateView(UpdateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        messages.success(self.request, _("User has been successfully changed")
-                         )
+        messages.success(self.request, _("User has been successfully changed"))
         return response
 
 
-def user_delete(request, pk):
-    user = get_object_or_404(User, pk=pk)
+class UserDeleteView(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        if user != request.user:
+            messages.error(request, _(
+                "You do not have the rights to delete another user."))
+            return redirect('user_list')
+        return render(request, 'users/user_delete.html', {'user': user})
 
-    if user != request.user:
-        messages.error(
-            request, _("You do not have the rights to change another user.")
-        )
-        return redirect('user_list')
+    def post(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        if user != request.user:
+            messages.error(request, _(
+                "You do not have the rights to delete another user."))
+            return redirect('user_list')
 
-    if request.method == 'POST':
         try:
             user.delete()
-            messages.success(request, _("User has been successfully deleted.")
-                             )
+            messages.success(request, _("User has been successfully deleted."))
         except ProtectedError:
-            messages.error(
-                request,
-                _("It is not possible to delete a user because it is in use.")
-            )
+            messages.error(request, _(
+                "It is not possible to delete a user because it is in use."))
         return redirect('user_list')
 
-    return render(request, 'users/user_delete.html', {'user': user})
 
-
-def user_list(request):
-    users = User.objects.all()
-    return render(request, 'users/users_list.html', {'users': users})
+class UserListView(LoginRequiredMixin, View):
+    def get(self, request):
+        users = User.objects.all()
+        return render(request, 'users/users_list.html', {'users': users})
